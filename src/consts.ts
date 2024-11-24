92b9391a0153a8e1db53fb23f9358b4bd29a1efb
yemo0 <yemo0@outlook.com>
2024-11-24 21:59:05 +0800

    fixed title

--- src/consts.ts
+++ src/consts.ts
@@ -4,6 +4,7 @@
 export const SITE_TITLE = "HOME";
 export const SITE_DESCRIPTION = "Welcome to my website!";
 export const WEB_SITE_URL = "http://localhost:3000/";
-
+export const GITHUB_PROFILE_URL = "https://github.com/yemo0/just-my-type-blog";
+export const DOCS_URL = "https://google.com";
 // Paginate
 export const PAGE_SIZE = 5;
